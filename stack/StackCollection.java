cedf76ec94b548daad1081cae92d92b0d88e0089
therudradev <rudragoh@gmail.com>
2025-08-05 18:51:58 +0530

    Stack using Collection

---
+++ stack/StackCollection.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+
+public class StackCollection {
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        s.push(40);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
