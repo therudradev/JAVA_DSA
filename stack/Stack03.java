8d26b6a4c7295b5f4905c0cb0b68261782ad54a6
therudradev <rudragoh@gmail.com>
2025-08-05 19:07:41 +0530

    Push at Bottom of Stack

---
+++ stack/Stack03.java
@@ -0,0 +1,32 @@
+package stack;
+// Push at Bottom of Stack
+import java.util.Stack;
+
+public class Stack03 {
+
+    public static void pushAtButtom(Stack<Integer> s, int data) {
+        if (s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtButtom(s, data);
+        s.push(top);
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        pushAtButtom(s, 4);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.pop());
+        }
+
+        // output:
+        // 3 2 1 4
+    }
+}
