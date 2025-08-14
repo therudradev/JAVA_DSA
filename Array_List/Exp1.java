dcc3c482fdb036645eb6668f3695f1af3fdd3bf9
therudradev <rudragoh@gmail.com>
2025-08-14 23:08:48 +0530

    Create an ArrayList

---
+++ Array_List/Exp1.java
@@ -0,0 +1,17 @@
+// Create an ArrayList
+package Array_List;
+
+import java.util.ArrayList;
+
+public class Exp1 {
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<String> list2 = new ArrayList<>();
+        ArrayList<Boolean> list3 = new ArrayList<>();
+
+        System.out.println(list);
+    }
+
+}
