a49c9c04ee173f503c6b8a52e8238f569e4eead4
therudradev <rudragoh@gmail.com>
2025-08-17 00:41:56 +0530

    Container with Most Water

---
+++ Array_List/Exp8.java
@@ -0,0 +1,18 @@
+/*
+ Container with Most Water
+ For given n lines on x-axis, use 2 lines to form a container such
+ that it holds maximum water.
+ height = [1,8,6,2,5,4,8,3,7]
+ */
+
+package Array_List;
+
+import java.util.ArrayList;
+
+public class Exp8 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
