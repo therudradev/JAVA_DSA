147218d3f7a5c7a85d691d8a72aceab3b142c8cc
therudradev <rudragoh@gmail.com>
2025-08-25 17:45:35 +0530

    Queue Reversal

---
+++ queue/Queue11.java
@@ -0,0 +1,35 @@
+package queue;
+//  Queue Reversal
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Queue11 {
+    public static void reverse(Queue<Integer> q) {
+        Stack<Integer> s = new Stack<>();
+
+        while (!q.isEmpty()) {
+            s.push(q.remove());
+        }
+
+        while (!s.isEmpty()) {
+            q.add(s.pop());
+        }
+    }
+
+    public static void main(String str[]) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(1);
+        q.add(2);
+        q.add(3);
+        q.add(4);
+        q.add(5);
+
+        reverse(q);
+
+        while (!q.isEmpty()) {
+            System.out.print(q.remove() + "  ");
+        }
+    }
+}
